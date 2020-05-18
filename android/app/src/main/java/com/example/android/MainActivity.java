4cb71abb2690b5bdd666c4b317dc07e32c6bc269
getupminaaa <getupmina0405@gmail.com>
2020-05-19 01:34:54 +0900

    Button을 누르면 Vibration이 울리도록 구현

--- android/app/src/main/java/com/example/android/MainActivity.java
+++ android/app/src/main/java/com/example/android/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.vibration_Button).setOnClickListener(onClickListener);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.vibration_Button:
+                    makeVibration();
+                    break;
+            }
+        }
+    };
+
+    private void makeVibration() {
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(1000); //1초 동안 진동
+    }
+
 }
