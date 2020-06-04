82927229579bfe709f7a2a22ecda614506207b6a
Jimen <hurjeesic@naver.com>
2020-06-05 06:36:14 +0900

    라즈베리파이와의 블루투스 통신 구현

--- android/app/src/main/java/com/example/android/MainActivity.java
+++ android/app/src/main/java/com/example/android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
@@ -12,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         findViewById(R.id.vibration_Button).setOnClickListener(onClickListener);
+        findViewById(R.id.bluetooth_button).setOnClickListener(onClickListener);
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
             switch (v.getId()) {
                 case R.id.vibration_Button:
                     makeVibration();
+                    break;
+                case  R.id.bluetooth_button:
+                    startActivity(new Intent(getApplicationContext(), BluetoothActivity.class));
                     break;
             }
         }
